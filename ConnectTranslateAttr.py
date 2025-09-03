1d50f4eaf636d38cc68d3bffae71aebba8e097f3
Lino <107348353+LinoPotato@users.noreply.github.com>
2025-09-03 21:09:22 +0800

    Add files via upload

---
+++ ConnectTranslateAttr.py
@@ -0,0 +1,15 @@
+import maya.cmds as cmds
+
+# Get the selection (driver first, driven second)
+sel = cmds.ls(sl=True)
+if len(sel) != 2:
+    cmds.error("Please select two objects: driver first, driven second.")
+
+driver = sel[0]
+driven = sel[1]
+
+#connectAttr() needs attribute names as strings in "nodeName.attributeName" 
+#format, so we have to build that string manually.
+
+# Connect translate attributes
+cmds.connectAttr(driver + ".translate", driven + ".translate", force=True)
